24d1d758fd80ad9f1feef7f9f032d215adacfa1d
Sumedh Ghaisas <sumedhghaisas@gmail.com>
2014-08-18 09:43:43 +0000

    * added LRUcache

---
+++ src/libgdl/core/cache/lru_cache.hpp
@@ -0,0 +1,156 @@
+/**
+ * @file lru_cache.hpp
+ * @author Sumedh Ghaisas
+ *
+ * Declaration of LRUCache.
+ */
+
+#ifndef _LIBGDL_CORE_LRU_CACHE_HPP_INCLUDED
+#define _LIBGDL_CORE_LRU_CACHE_HPP_INCLUDED
+
+#include <boost/function.hpp>
+#include <boost/unordered_map.hpp>
+
+namespace libgdl
+{
+
+/**
+ * LRU Cache module.
+ * Hit time : around 13 microseconds (highly dependent on the machine and the
+                                      compiler)
+ *
+ * key_type should implement a getHash() function which returns a size_t hash
+ * value or overload of Get function can be used which accepts a function
+ * pointer of hash calculation function. This hash calculation function should
+ * take const key_type as input and should return size_t (hash value).
+ *
+ * Constructor accepts a boost function as a default function to call when there
+ * is a miss. This default miss function can be overridden with appropriate Get
+ * function overload. The function should accept a constant Key_type reference
+ * and return pointer to the value_type object. Member functions can be passed
+ * by binding. (for example see the use of LRUCache in GDL class)
+ *
+ * @code
+ * boost::function<B* (const A&)> f(boost::bind(&A::GetVal, this, _1))
+ * LRUCache<A, B> cache(f, 1024);
+ * A test;
+ * B* temp = cache.Get(test);
+ * @endcode
+ *
+ */
+
+template<class key_type, class value_type>
+class LRUCache
+{
+ public:
+  //! Constructor
+  //!
+  //! \param default_f const boost::function<value_type* (const key_type&)>&
+  //! : default miss calling function
+  //! \param capacity unsigned short : number of entries to cache
+  //!
+  //!
+  LRUCache(const boost::function<value_type* (const key_type&)> default_f,
+           unsigned short capacity = 1024);
+
+  //! Constructor
+  //!
+  //! \param unsigned short capacity = 1024
+  //!
+  //!
+  LRUCache(unsigned short capacity = 1024);
+
+  //! Returns the value associated with the given key.
+  //! The key is identified by its hash value obtained by function getHash()
+  //! Default function is called if miss.
+  //!
+  //! \param key const key_type&
+  //! \return value_type*
+  //!
+  //!
+  value_type* Get(const key_type& key);
+
+  //! Returns the value associated with the given key.
+  //! The key is identified by its hash value obtained by function getHash()
+  //! Given function is called if miss.
+  //!
+  //! \param key const key_type&
+  //! \param f_override const boost::function<value_type* (const key_type&)>&
+  //!                                 : override for default miss function
+  //! \return value_type*
+  //!
+  //!
+  value_type* Get(const key_type& key,
+                  boost::function<value_type* (const key_type&)>& f_override);
+
+  //! Returns the value associated with the given key.
+  //! The key is identified by its hash value obtained by boost function
+  //! given as the third argument
+  //! Given miss function is called if miss is detected.
+  //!
+  //! \param key const key_type&
+  //! \param f_override const boost::function<value_type* (const key_type&)>&
+  //!         : override for default miss function
+  //! \param hash_funct_override const boost::function<size_t (const key_type&)>&
+  //!         : function to compute hash
+  //! \return value_type*
+  //!
+  //!
+  value_type* Get(const key_type& key,
+                  boost::function<value_type* (const key_type&)>& f_override,
+                  boost::function<size_t (const key_type&)>& hash_funct_override);
+
+  value_type* Query(const key_type& key);
+
+  value_type* Query(const key_type& key,
+                    boost::function<size_t (const key_type&)>& hash_funct_override);
+
+  //! sets cache is with default miss function
+  //!
+  //! \param key_type& boost::function<value_type* (const
+  //! \return void
+  //!
+  //!
+  void SetDefaultFunction(boost::function<value_type* (const key_type&)> default_f);
+
+  Log& GetLog() { return log; }
+
+ private:
+  //! default function to call when miss detected
+  const boost::function<value_type* (const key_type&)> default_f;
+
+  //! maximum entries to cache
+  short capacity;
+
+  //! array storing hash of the cached keys
+  uint64_t *hashs;
+
+  //! array of pointers pointing towards cached values
+  value_type **values;
+
+  //! basic implementation of circular link list (used for LRU)
+  unsigned short* forward_pointing;
+  unsigned short* backward_pointing;
+
+  //! mapping of hash to slot in which value is stored
+  boost::unordered_map<size_t, unsigned short> slot_map;
+
+  //! provides the next empty location (used until cache is not full)
+  unsigned short next_empty;
+
+  //! status of the cache (full or not)
+  bool isFull;
+
+  //! does default function exist
+  bool isInitialized;
+
+  //! logging stream
+  mutable Log log;
+}; // class LRUache
+
+}; // namespace libgdl
+
+
+#include "lru_cache_impl.hpp"
+
+#endif // _EPICURUS_CORE_LRU_CACHE_HPP_INCLUDED
---
+++ src/libgdl/core/cache/lru_cache_impl.hpp
@@ -0,0 +1,212 @@
+/**
+ * @file lru_cache_impl.hpp
+ * @author Sumedh Ghaisas
+ *
+ * Implementation of LRUCache.
+ */
+#include <boost/bind.hpp>
+
+namespace libgdl
+{
+
+template<class key_type, class value_type>
+LRUCache<key_type, value_type>::
+  LRUCache(const boost::function<value_type* (const key_type&)> f,
+           unsigned short capacity)
+        : default_f(f), capacity(capacity), isInitialized(true)
+{
+  hashs = new size_t[capacity];
+  values = new value_type*[capacity];
+  forward_pointing = new unsigned short[capacity];
+  backward_pointing = new unsigned short[capacity];
+
+  // 0th index is used as the loopback index
+  next_empty = 1;
+  isFull = false;
+
+  // initialize all the slots
+  for(size_t i = 0;i < capacity;i++)
+  {
+    forward_pointing[i] = 0;
+    backward_pointing[i] = 0;
+  }
+}
+
+template<class key_type, class value_type>
+LRUCache<key_type, value_type>::LRUCache(unsigned short capacity)
+        : default_f(), capacity(capacity), isInitialized(false)
+{
+  hashs = new size_t[capacity];
+
+  values = new value_type*[capacity];
+  for(size_t i = 0;i < capacity;i++) values[i] = NULL;
+
+  forward_pointing = new unsigned short[capacity];
+  backward_pointing = new unsigned short[capacity];
+
+  // 0th index is used as the loopback index
+  next_empty = 1;
+  isFull = false;
+
+  // initialize all the slots
+  for(size_t i = 0;i < capacity;i++)
+  {
+    forward_pointing[i] = 0;
+    backward_pointing[i] = 0;
+  }
+}
+
+template<class key_type, class value_type>
+value_type* LRUCache<key_type, value_type>::Get(const key_type& keyin)
+{
+  if(!isInitialized)
+  {
+    log.Fatal << "Cache is not initialized with default function." << std::endl;
+    return NULL;
+  }
+
+  boost::function<size_t (const key_type&)>
+      hash_funct(boost::bind(key_type::getHash, &keyin));
+  return Get(keyin, default_f, hash_funct);
+}
+
+template<class key_type, class value_type>
+value_type* LRUCache<key_type, value_type>::
+  Get(const key_type& keyin,
+      boost::function<value_type* (const key_type&)>& f_override)
+{
+  boost::function<size_t (const key_type&)>
+      hash_funct(boost::bind(key_type::getHash, &keyin));
+  return Get(keyin, f_override, hash_funct);
+}
+
+template<class key_type, class value_type>
+value_type* LRUCache<key_type, value_type>::
+  Get(const key_type& keyin,
+      boost::function<value_type* (const key_type&)>& f_override,
+      boost::function<size_t (const key_type&)>& hash_funct)
+{
+  // get hash value of the key
+  size_t hash = hash_funct(keyin);
+
+  // check for a hit
+  boost::unordered_map<size_t, unsigned short>::const_iterator it =
+                                                      slot_map.find(hash);
+
+  // if it is a hit
+  if(it != slot_map.end())
+  {
+    const short& slot = it->second;
+    const short& next = forward_pointing[slot];
+    const short& back = backward_pointing[slot];
+
+    // remove the element from the link list and join the back and next element
+    forward_pointing[back] = next;
+    backward_pointing[next] = back;
+
+    // put the element at the start
+    forward_pointing[slot] = forward_pointing[0];
+    backward_pointing[forward_pointing[0]] = slot;
+    forward_pointing[0] = slot;
+    backward_pointing[slot] = 0;
+
+    // return the value associated with that slot
+    return values[slot];
+  }
+  // if it is a miss but cache is not yet full
+  else if(!isFull)
+  {
+    // get next non empty slot and increment the next_empty
+    const short slot = next_empty++;
+
+    // flag the cache being full if next_empty equals capacity
+    if(next_empty == capacity) isFull = true;
+
+    // put hash value of the key into the slot
+    hashs[slot] = hash;
+    // put the entry of the key in the map
+    slot_map[hash] = slot;
+
+    // put the newly entry at the start of the link list
+    forward_pointing[slot] = forward_pointing[0];
+    backward_pointing[forward_pointing[0]] = slot;
+    forward_pointing[0] = slot;
+    backward_pointing[slot] = 0;
+
+    // call miss function and update the value
+    value_type* temp = f_override(keyin);
+    values[slot] = temp;
+    // return the calculated value
+    return temp;
+  }
+  else
+  {
+    // the the least recently used position
+    // which is the tail of the link list
+    const short& slot = backward_pointing[0];
+    const short& next = forward_pointing[slot];
+    const short& back = backward_pointing[slot];
+
+    // erase the entry against that slot from the map
+    slot_map.erase(slot_map.find(hashs[slot]));
+
+    // update the hash of the provided key in that slot
+    hashs[slot] = hash;
+    // add the entry of that slot in the map against new hash
+    slot_map[hash] = slot;
+
+    // remove the element from the link list and join the back and next element
+    forward_pointing[back] = next;
+    backward_pointing[next] = back;
+
+    // add the entry at the start of the link list
+    forward_pointing[slot] = forward_pointing[0];
+    backward_pointing[forward_pointing[0]] = slot;
+    forward_pointing[0] = slot;
+    backward_pointing[slot] = 0;
+
+    // call the miss function and get the update the value
+    value_type* temp = f_override(keyin);
+    // delete the existing value
+    delete values[slot];
+    // replace it with new value
+    values[slot] = temp;
+    // return the calculated value
+    return temp;
+  }
+  return NULL; // to avoid warning
+}
+
+template<class key_type, class value_type>
+value_type* LRUCache<key_type, value_type>::Query(const key_type& keyin)
+{
+  boost::function<size_t (const key_type&)>
+      hash_funct(boost::bind(key_type::getHash, &keyin));
+  return Query(keyin, hash_funct);
+}
+
+template<class key_type, class value_type>
+value_type* LRUCache<key_type, value_type>::
+  Query(const key_type& key,
+        boost::function<size_t (const key_type&)>& hash_funct)
+{
+  // get hash value of the key
+  size_t hash = hash_funct(key);
+
+  // check for a hit
+  boost::unordered_map<size_t, unsigned short>::const_iterator it =
+                                                          slot_map.find(hash);
+
+  if(it != slot_map.find(hash)) return values[it->second];
+  else return NULL;
+}
+
+template<class key_type, class value_type>
+void LRUCache<key_type, value_type>::
+    SetDefaultFunction(boost::function<value_type* (const key_type&)> f)
+{
+  isInitialized = true;
+  default_f = f;
+}
+
+}; // namespace libgdl
