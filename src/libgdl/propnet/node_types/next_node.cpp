44f5379996bdd3c8e9799da505cbc0c6f27de798
= <sumedhghaisas@gmail.com>
2015-03-03 01:56:09 +0530

    * Opti test

--- src/libgdl/propnet/node_types/next_node.cpp
+++ src/libgdl/propnet/node_types/next_node.cpp
@@ -5,6 +5,7 @@
 
 #include "../entry_manager.hpp"
 #include "../entry_types/save_entry.hpp"
+#include "../entry_types/or_entry.hpp"
 
 using namespace std;
 using namespace libgdl::propnet;
@@ -21,16 +22,23 @@ tuple<bool, size_t> NextNode::CodeGen(EntryManager& em, size_t v_stamp)
 
   if(!isVisited)
   {
-    size_t out = em.GetNewID();
+    size_t or_entry_id = em.GetNewID();
 
     list<tuple<bool, size_t>> in_ids;
 
     for(auto n : in_degree)
     {
       auto t_entry = n->CodeGen(em, v_stamp);
-      em.AddStamp(get<1>(t_entry), out);
+      em.AddStamp(get<1>(t_entry), or_entry_id);
       in_ids.push_back(t_entry);
     }
+    em.AddEntry(new OrEntry(or_entry_id, in_ids));
+
+    size_t out = em.GetNewID();
+    em.AddStamp(or_entry_id, out);
+
+    in_ids.clear();
+    in_ids.emplace_back(true, or_entry_id);
 
     em.AddEntry(new SaveEntry(out, id, in_ids));
 
